4bb088fad3beaead468883a178ad13420a0aceb3
Martha Cryan <martha.cryan@ibm.com>
2020-07-28 15:58:19 -0500

    Add more baseline tests

--- packages/toc/test/toc.spec.ts
+++ packages/toc/test/toc.spec.ts
@@ -4,41 +4,114 @@
 import 'jest';
 
 // import { Widget } from '@lumino/widgets';
+import { Context } from '@jupyterlab/docregistry';
 
+import {
+  INotebookModel,
+  NotebookPanel,
+  NotebookTracker
+} from '@jupyterlab/notebook';
+import { initNotebookContext } from '@jupyterlab/testutils';
 import { DocumentManager, IDocumentManager } from '@jupyterlab/docmanager';
-import { TableOfContents } from '@jupyterlab/toc';
+import * as ToC from '@jupyterlab/toc';
 import { RenderMimeRegistry } from '@jupyterlab/rendermime';
+import { ServiceManager } from '@jupyterlab/services';
 import { DocumentRegistry, TextModelFactory } from '@jupyterlab/docregistry';
+import { JupyterServer } from '@jupyterlab/testutils/lib/start_jupyter_server';
 
-import * as Mock from '@jupyterlab/testutils/lib/mock';
+import { NBTestUtils, Mock } from '@jupyterlab/testutils';
 
-describe('ToC/index', () => {
+const server = new JupyterServer();
+
+beforeAll(async () => {
+  jest.setTimeout(20000);
+  await server.start();
+});
+
+afterAll(async () => {
+  await server.shutdown();
+});
+
+describe('@jupyterlab/toc', () => {
   let manager: IDocumentManager;
+  let widget: ToC.TableOfContents;
+  let registry: DocumentRegistry;
+  let serviceManager: ServiceManager.IManager;
+  let factory: TextModelFactory;
+  let context: Context<INotebookModel>;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     const opener: DocumentManager.IWidgetOpener = {
       open: widget => {
         /* no op */
       }
     };
-    let registry = new DocumentRegistry({
-      textModelFactory: new TextModelFactory()
+    factory = new TextModelFactory();
+    registry = new DocumentRegistry({
+      textModelFactory: factory
     });
-    let serviceManager = new Mock.ServiceManagerMock();
+    serviceManager = new Mock.ServiceManagerMock();
     manager = new DocumentManager({
       registry,
       opener,
       manager: serviceManager
     });
+    context = await initNotebookContext({ manager: serviceManager, startKernel: true });
+    context.model.fromJSON(NBTestUtils.DEFAULT_CONTENT);
+    await serviceManager.contents.save(context.path, {
+      type: 'notebook',
+      format: 'json',
+      content: NBTestUtils.DEFAULT_CONTENT
+    });
   });
+
+  afterAll(async () => {
+    await context.sessionContext.shutdown();
+    context.dispose();
+  });
+
   describe('TableOfContents', () => {
-    describe('#constructor()', () => {
+    describe('#constructor', () => {
       it('should construct a new ToC widget', () => {
-        const widget = new TableOfContents({
+        widget = new ToC.TableOfContents({
           docmanager: manager,
           rendermime: new RenderMimeRegistry()
         });
-        expect(widget).toBeInstanceOf(TableOfContents);
+        expect(widget).toBeInstanceOf(ToC.TableOfContents);
+      });
+    });
+
+    describe('#current', () => {
+      let notebookWidget: NotebookPanel;
+      let registry: ToC.TableOfContentsRegistry;
+      let notebookGenerator: ToC.TableOfContentsRegistry.IGenerator<NotebookPanel>;
+      let tracker: NotebookTracker;
+
+      it('should create a registry', () => {
+        registry = new ToC.TableOfContentsRegistry();
+      });
+
+      it ('should create a notebook generator', () => {
+        tracker = new NotebookTracker({
+          namespace: 'notebook'
+        });
+        notebookGenerator = ToC.createNotebookGenerator(
+          tracker,
+          widget,
+          NBTestUtils.defaultRenderMime().sanitizer
+        );
+      });
+
+      it ('should add a notebook generator to the registry', () => {
+        registry.add(notebookGenerator);
+      });
+
+      it ('should find the notebook generator', async () => {
+        notebookWidget = NBTestUtils.createNotebookPanel(context);
+        expect(notebookWidget).toBeInstanceOf(NotebookPanel);
+        tracker.add(notebookWidget);
+        const foundNotebookGenerator = registry.find(notebookWidget);
+        expect(foundNotebookGenerator).toBeDefined();
       });
     });
   });
